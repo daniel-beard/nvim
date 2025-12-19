78fe4a7ae770fe27a3eccc98da9ffca7bded5beb
Daniel Beard <daniel.beard@splitwise.com>
2025-12-19 11:41:59 -0800

    Simplify chadrc, move restore cursor position autocommand to correct file

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -7,50 +7,29 @@ local M = {}
 
 M.base46 = {
 	theme = "catppuccin",
-
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
 }
 
- -- Restore cursor last position upon reopening the file 
-vim.cmd([[ 
-   augroup last_cursor_position 
-   autocmd! 
-   autocmd BufReadPost * 
-   \ if line("'\"") > 1 && line("'\"") <= line("$") && &ft !~# 'commit' | execute "normal! g`\"zvzz" | endif 
-   augroup END 
-]])
-
-local function run_make()
-  vim.cmd[[silent make]]
-  if #(vim.fn.getqflist()) > 0 then
-	  vim.cmd.copen()
-  else
-	  vim.cmd.cclose()
-  end
-end
-
-vim.api.nvim_create_user_command(
-  'Periphery',
-  function()
-    vim.opt_local.makeprg = "periphery scan"
-    vim.opt_local.errorformat = {
-      "%f:%l:%c: %tarning: %m"
-    }
-    run_make()
-  end,
-  {
-    desc = "Run periphery and display results in quickfixlist"
-  }
-)
-
--- M.nvdash = { load_on_startup = true }
--- M.ui = {
---       tabufline = {
---          lazyload = false
---      }
--- }
+-- local function run_make()
+--   vim.cmd[[silent make]]
+--   if #(vim.fn.getqflist()) > 0 then
+-- 	  vim.cmd.copen()
+--   else
+-- 	  vim.cmd.cclose()
+--   end
+-- end
+--
+-- vim.api.nvim_create_user_command(
+--   'Periphery',
+--   function()
+--     vim.opt_local.makeprg = "periphery scan"
+--     vim.opt_local.errorformat = {
+--       "%f:%l:%c: %tarning: %m"
+--     }
+--     run_make()
+--   end,
+--   {
+--     desc = "Run periphery and display results in quickfixlist"
+--   }
+-- )
 
 return M
