3881c62785550edbab3c1d3dc0454b1fddeda92d
daniel-beard <daniel.beard0@gmail.com>
2026-01-05 15:18:14 -0800

    Stack run/test term mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,6 +3,7 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
+local term = require("nvchad.term")
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 -- Remap nvim's default-mapping that maps Y to y$
@@ -14,3 +15,10 @@ map("i", "jk", "<ESC>")
 
 map("v", ">", ">gv", { desc = "Indent" })
 
+-- Terminal
+map("n", "<leader>st", function()
+  term.toggle { pos = "vsp", size = 0.4, cmd = "stack test --file-watch", id = "stack_test" } 
+end, { desc = "Run `stack test --file-watch` in a term window" })
+map("n", "<leader>sr", function()
+  term.runner { pos = "float", cmd = "clear && stack run", id = "stack_run" }
+end, { desc = "Run `stack run` in a term" })
